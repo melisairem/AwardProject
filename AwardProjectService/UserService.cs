ae1d9fea73fc2b0064ca3eeebafb20818f1346e0
melisairem <melisairemcik0505@gmail.com>
2024-09-17 01:30:05 +0300

    PasswordHash

--- AwardProjectService/UserService.cs
+++ AwardProjectService/UserService.cs
@@ -1,9 +1,15 @@
 ﻿using AwardProjectEntity;
 using Dal;
+using Utility.Security;
 
 namespace AwardProjectService
 {
     public class UserService : BaseCrudDal<User>
     {
+        public override void Add(User entity)
+        {
+            entity.Password = PasswordHash.Hash(entity.Password);
+            base.Add(entity);
+        }
     }
 }
--- AwardProjectWeb/Controllers/HomeController.cs
+++ AwardProjectWeb/Controllers/HomeController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq.Expressions;
+using AwardProjectEntity;
+using AwardProjectService;
+using Microsoft.AspNetCore.Mvc;
+using Utility.Security;
 
 namespace AwardProjectWeb.Controllers;
 
 public class HomeController : Controller
 {
+    private UserService _userService;
+
+    public HomeController(UserService userService)
+    {
+        _userService = userService;
+    }
+
     public IActionResult Index()
     {
+        string email = "melisa@gmail.com";
+        string password = "321";
+
+        User user = _userService.GetAll(predicates: new List<Expression<Func<User, bool>>>
+        {
+            i => i.Email == email
+        }).FirstOrDefault();
+
+        if (user != null)
+        {
+            bool isCorrectPassword = PasswordHash.Verify(user.Password, password);
+        }
+        
         return View();
     }
 }
